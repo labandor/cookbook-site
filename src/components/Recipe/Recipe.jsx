af3371d07d9da2c3b35a4cd7d74f3735638ad458
Laband <lukeabandeen@gmail.com>
2024-03-18 15:56:20 -0400

    added substance to Recipe.jsx

---
+++ src/components/Recipe/Recipe.jsx
@@ -0,0 +1,15 @@
+import './Recipe.css';
+import { link } from 'react-router-dom';
+
+const Recipe = (rec) => {
+    return (
+       	<>
+            <Link className="recipe" to={`/recipe/${rec._id}`}>
+                <img className="recipe-image" src={rec.imgURL} alt={rec.name} />
+                <div className="recipe-name">{rec.name}</div>
+            </Link>
+        </>
+    )
+}
+export default Recipe;
+}
