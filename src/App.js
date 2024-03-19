737a47cca7c664b87a81ae38fbadc2a43fce3414
Laband <lukeabandeen@gmail.com>
2024-03-19 09:44:39 -0400

    added routes and user based useState/useEffect to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav.jsx';
+import Footer from './components/Footer.jsx';
+import About from './screens/About/About.jsx';
+import Home from './screens/Home/Home.jsx';
+import Browse from './screens/Browse/Browse.jsx';
+import Create from './screens/Create/Create.jsx';
+import Details from './screens/Details/Details.jsx';
+import Edit from './screens/Edit/Edit.jsx';
+import SignIn from './screens/SignIn/SignIn.jsx';
+import SignOut from './screens/SignOut/SignOut.jsx';
+import SignUp from './screens/SignUp/SignUp.jsx';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { verifyUser } from './services/user.js';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	  <Nav />
+	  <Routes>
+	  	<Route path="/" element={<Home />} />
+		<Route path="/sign-up" element={<SignUp setUser={setUser} />} />
+        	<Route path="/sign-in" element={<SignIn setUser={setUser} />} />
+        	<Route path="/sign-out" element={<SignOut setUser={setUser} />} />
+	  	<Route path="/browse" element={<Browse />} />
+	  	<Route path="/recipe/:id" element={<Details user={user} />} />
+	 	<Route path="/add-recipe" element={user ? <Create user={user} /> : <Navigate to="/sign-up" />} />
+	  	<Route path="/recipe/:id/edit" element={user ? <Edit user={user} /> : <Navigate to="/" />} />
+	  </Routes>
     </div>
   );
 }
