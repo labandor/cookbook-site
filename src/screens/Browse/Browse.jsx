d04cc2126b457733b56022387d7c4eb7f5533b02
Laband <lukeabandeen@gmail.com>
2024-03-19 11:56:05 -0400

    super baseline framework for Browse decided to add search before diving farther in

---
+++ src/screens/Browse/Browse.jsx
@@ -0,0 +1,11 @@
+import Recipe from '../../components/Recipe/Recipe.jsx';
+import './Browse.css';
+import { getRecipes } from '../../services/recipes.js';
+import { useState, useReact } from 'react';
+
+function Browse() {
+	return(
+	);
+}
+
+export default Browse;
