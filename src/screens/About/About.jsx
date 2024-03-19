04d0fff6dfd23dc895623ce6a18c2a290fd12b2b
Laband <lukeabandeen@gmail.com>
2024-03-19 11:48:22 -0400

    created framwork for about.jsx

---
+++ src/screens/About/About.jsx
@@ -0,0 +1,28 @@
+import react from 'react';
+import './About.css';
+
+function About() {
+	return(
+		<div>
+			<h1>About Us</h1>
+			<div id="Booker">
+				<h2>Booker Ngoon</h2>
+				<p> insert here </p>
+			</div>
+			<div id="Aleksandre">
+				<h2>Aleksandre Chkhikvishvili</h2>
+				<p> input here </p>
+			</div>
+			<div id="Albert">
+				<h2>Albert Rocha</h2>
+				<p> place here </p>
+			</div>
+			<div id="Luke">
+				<h2>Luke Bandeen</h2>
+				<p> plop here </p>
+			</div>
+		</div>
+	);
+}
+
+export default About;
