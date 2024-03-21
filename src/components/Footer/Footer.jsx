eba22bb286bbaa4f9157d6fc3aaa331944f5b1fd
Booker Ngoon <bookerngoon@gmail.com>
2024-03-21 09:33:07 -0400

    why am i broken

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import './Footer.css'
 
 const Footer = () =>
 	<footer>
-		<p> Brought to you by Booker, Aleksandre, Albert, and Luke </p>
+		<p> Brought to you by Booker, Aleksandre, , and Luke </p>
 	</footer>
 
 export default Footer
