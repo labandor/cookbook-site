7f4eeba1b26aef0c6e810755b91fc7c78c0ae7b2
aleksandre <aleksandre.chkhikvishvili1@gmail.com>
2024-03-21 18:04:09 -0400

    added more and more and more and more css

--- src/screens/Browse/Browse.jsx
+++ src/screens/Browse/Browse.jsx
@@ -1,3 +1,55 @@
+// import Recipe from '../../components/Recipe/Recipe.jsx';
+// import './Browse.css';
+// import { getRecipes } from '../../services/recipes.js';
+// import { useState, useEffect } from 'react';
+// import Search from '../../components/Search/Search.jsx';
+
+// function Browse() {
+// 	const [recipes, setRecipes] = useState([]);
+//   	const [searchResult, setSearchResult] = useState([]);
+
+//   	useEffect(() => {
+//   		const fetchRecipe = async () => {
+//       			const allRecipes = await getRecipes()
+//       			setRecipes(allRecipes)
+//       			setSearchResult(allRecipes)
+//     		}
+//     			fetchRecipe()
+//   	}, []);
+
+// 	const handleSearch = (event) => {
+// 		const results = recipes.filter((recipe) =>
+// 	     		recipe.title.toLowerCase().includes(event.target.value.toLowerCase())
+//     		)
+//     		setSearchResult(results)
+//   	}
+
+//  	const handleSubmit = (event) => event.preventDefault()
+
+// 	return(
+//       		<div className='recipes'>
+// 			<Search onSubmit={handleSubmit} handleSearch={handleSearch} />
+//         		{searchResult.map((recipe, index) => {
+//           			return (
+//             				<Recipe
+//               					_id={recipe._id}
+// 						key={index}
+//               					title={recipe.title}
+//               					image={recipe.image}
+// 						summary={recipe.summary}
+// 						steps={recipe.steps}
+// 						cuisines={recipe.cuisines}
+// 						sourceURL = {recipe.sourceURL}
+//             				/>
+//           			)
+//         		})}
+//       		</div>
+// 	);
+// }
+
+// export default Browse;
+
+
 import Recipe from '../../components/Recipe/Recipe.jsx';
 import './Browse.css';
 import { getRecipes } from '../../services/recipes.js';
@@ -5,46 +57,49 @@ import { useState, useEffect } from 'react';
 import Search from '../../components/Search/Search.jsx';
 
 function Browse() {
-	const [recipes, setRecipes] = useState([]);
-  	const [searchResult, setSearchResult] = useState([]);
+    const [recipes, setRecipes] = useState([]);
+    const [searchResult, setSearchResult] = useState([]);
 
-  	useEffect(() => {
-  		const fetchRecipe = async () => {
-      			const allRecipes = await getRecipes()
-      			setRecipes(allRecipes)
-      			setSearchResult(allRecipes)
-    		}
-    			fetchRecipe()
-  	}, []);
+    useEffect(() => {
+        const fetchRecipe = async () => {
+            const allRecipes = await getRecipes()
+            setRecipes(allRecipes)
+            setSearchResult(allRecipes)
+        }
+        fetchRecipe()
+    }, []);
 
-	const handleSearch = (event) => {
-		const results = recipes.filter((recipe) =>
-	     		recipe.title.toLowerCase().includes(event.target.value.toLowerCase())
-    		)
-    		setSearchResult(results)
-  	}
+    const handleSearch = (event) => {
+        const results = recipes.filter((recipe) =>
+            recipe.title.toLowerCase().includes(event.target.value.toLowerCase())
+        )
+        setSearchResult(results)
+    }
 
- 	const handleSubmit = (event) => event.preventDefault()
+    const handleSubmit = (event) => event.preventDefault()
 
-	return(
-      		<div className='recipes'>
-			<Search onSubmit={handleSubmit} handleSearch={handleSearch} />
-        		{searchResult.map((recipe, index) => {
-          			return (
-            				<Recipe
-              					_id={recipe._id}
-						key={index}
-              					title={recipe.title}
-              					image={recipe.image}
-						summary={recipe.summary}
-						steps={recipe.steps}
-						cuisines={recipe.cuisines}
-						sourceURL = {recipe.sourceURL}
-            				/>
-          			)
-        		})}
-      		</div>
-	);
+    return(
+        <div className='recipes'>
+            <Search onSubmit={handleSubmit} handleSearch={handleSearch} />
+            <div className="recipe-container">
+                {searchResult.map((recipe, index) => {
+                    return (
+                        <div className="recipe-item" key={index}>
+                            <Recipe
+                                _id={recipe._id}
+                                title={recipe.title}
+                                image={recipe.image}
+                                summary={recipe.summary}
+                                steps={recipe.steps}
+                                cuisines={recipe.cuisines}
+                                sourceURL={recipe.sourceURL}
+                            />
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default Browse;
