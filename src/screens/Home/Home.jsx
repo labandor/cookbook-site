05840780ba3510055b4dbbf6efbaa6e508c7fb81
aleksandre <aleksandre.chkhikvishvili1@gmail.com>
2024-03-22 15:31:30 -0400

    done with home page

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,3 +1,5 @@
+
+
 import './Home.css'
 import Recipe from '../../components/Recipe/Recipe.jsx';
 
@@ -6,8 +8,10 @@ const Home = (props) => {
       <div className='home'>
         {/* <img className='image' src="https://images.unsplash.com/photo-1483137140003-ae073b395549?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /> */}
         <Recipe />
-	<h1>Chefs Kiss</h1>
-	<p>We set out to create a site to be a hub of recipes that can be shared accross cultures to connect people and satisfy our tastebuds</p>
+        <div className="content">
+          <h1>Chefs Kiss</h1>
+          <p>We set out to create a site to be a hub of recipes that can be shared across cultures to connect people and satisfy our tastebuds</p>
+        </div>
       </div>
   )
 }
