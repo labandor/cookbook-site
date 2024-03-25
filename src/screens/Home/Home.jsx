625bc1661edc9cdaee6a8d5edfb15e0deaa5e91d
Laband <lukeabandeen@gmail.com>
2024-03-25 09:35:40 -0400

    maybe fixed home page error

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -5,7 +5,6 @@ const Home = (props) => {
   return (
       <div className='home'>
         {/* <img className='image' src="https://images.unsplash.com/photo-1483137140003-ae073b395549?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /> */}
-        <Recipe />
         <div className="content">
           <h1>Chefs Kiss</h1>
           <p>We set out to create a site to be a hub of recipes that can be shared across cultures to connect people and satisfy our tastebuds</p>
